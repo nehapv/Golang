3d053eb2dfe7fddb7477a4f5cb3f76738987a713
Neha <nehaprakash398@gmail.com>
2020-10-24 21:34:20 +0530

    one

--- P5.go
+++ P5.go
@@ -3,16 +3,16 @@ package main
 
 import "fmt"
 
-var area int // Variable declare outside the main function
+var area int
 
 func main() {
-	var l, b int //Declaration of multiple Variables
+	var l, b int
 	fmt.Print("Enter Length of Rectangle : ")
 	fmt.Scan(&l)
 	fmt.Print("Enter Breadth of Rectangle : ")
 	fmt.Scan(&b)
 	area = l * b
-	fmt.Println("Area of Rectangle : ", area) //move to new line
+	fmt.Println("Area of Rectangle : ", area)
 
 	fmt.Print("Enter Length of Square : ")
 	fmt.Scan(&l)
